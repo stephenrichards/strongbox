1d6a56b220f027f08d5492ca28be7d5bc67ba9e7
Stephen Richards <stephen@stephenrichards.eu>
2013-04-07 18:18:13 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strongbox::Application.config.secret_token = 'f1b87ba2cfad4e70a01e5bf123251bdb809c7293b0477ff0cd9f78d5861db7a0a64e5cae24ce497697709f68a8740d9557b15a9f46f5577b81424513600667c1'
